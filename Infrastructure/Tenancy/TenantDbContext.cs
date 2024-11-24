68a45c96052072e2386d0714f2b49fc78d4c1e9a
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-11-24 17:30:24 +0200

    Add TenantDbContext for multi-tenancy support
    - Introduced `TenantDbContext` class inheriting from `EFCoreStoreDbContext<SchoolTenantInfo>`.
    - Added namespace `Infrastructure.Tenancy` to `TenantDbContext.cs`.
    - Included using directives for `Finbuckle.MultiTenant.Stores` and `Microsoft.EntityFrameworkCore`.

---
+++ Infrastructure/Tenancy/TenantDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Tenancy;
+
+using Finbuckle.MultiTenant.Stores;
+using Microsoft.EntityFrameworkCore;
+
+public class TenantDbContext(DbContextOptions<TenantDbContext> options) 
+    : EFCoreStoreDbContext<SchoolTenantInfo>(options){}
