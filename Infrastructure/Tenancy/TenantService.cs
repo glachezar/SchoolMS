98fca4405c8a57e1e3777407f1529ec499327bd8
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-11-24 18:19:07 +0200

    Add TenantService class and CreateTenantAsync method
    Introduce Infrastructure.Tenancy namespace and add TenantService class implementing ITenantService. Add CreateTenantAsync method with NotImplementedException. Include necessary using directives.

---
+++ Infrastructure/Tenancy/TenantService.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Tenancy;
+
+using Application.Interfaces.Tenancy;
+using Application.Interfaces.Tenancy.Commands;
+using System.Threading.Tasks;
+
+internal class TenantService : ITenantService
+{
+    public Task<string> CreateTenantAsync(CreateTenantRequest createTenant)
+    {
+        throw new NotImplementedException();
+    }
+}
