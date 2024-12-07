5854319720bf96a4b6b2f7b8402fb11746de7fef
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2024-12-07 12:25:55 +0200

    Add token service and update project structure
    Updated `Application.csproj` to change folder path from `Interfaces\Tenancy\Queries\` to `Features\Tenancy\Queries\`.
    
    Added `ITokenService` interface with `LoginAsync` and `RefreshTokenAsync` methods in `Application.Features.Identity.Tokens`.
    
    Added `RefreshTokenRequest` class with `CurrentJwtToken` and `CurrentRefreshToken` properties in `Application.Features.Identity.Tokens`.
    
    Added `TokenRequest` class with `Email` and `Password` properties, and a commented-out `RefreshToken` property in `Application.Features.Identity.Tokens`.
    
    Added `TokenResponse` class with `JwtToken`, `RefreshToken`, and `RefreshTokenExpiryDate` properties in `Application.Features.Identity.Tokens`.

---
+++ Application/Features/Identity/Tokens/TokenResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Features.Identity.Tokens;
+
+public class TokenResponse
+{
+    public string JwtToken { get; set; }
+
+    public string RefreshToken { get; set; }
+
+    public DateTime RefreshTokenExpiryDate { get; set; }
+}
