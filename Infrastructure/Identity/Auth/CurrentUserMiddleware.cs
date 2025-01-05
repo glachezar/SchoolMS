a7dc5866a3487cc1f58101fa62c733cfd09c5e76
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2025-01-05 20:31:35 +0200

    Add CurrentUserMiddleware to set current user in pipeline
    Introduced a new namespace `Infrastructure.Identity.Auth`.
    Added `CurrentUserMiddleware` class implementing `IMiddleware`.
    The middleware sets the current user using `ICurrentUserService`
    based on `HttpContext.User` before calling the next middleware.
    Included necessary using directives for the new functionality.

---
+++ Infrastructure/Identity/Auth/CurrentUserMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Identity.Auth;
+
+using Application.Features.Identity.Users;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+public class CurrentUserMiddleware : IMiddleware
+{
+    readonly ICurrentUserService _currentUserService;
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        _currentUserService.SetCurrentUser(context.User);
+        await next(context);
+    }
+}
