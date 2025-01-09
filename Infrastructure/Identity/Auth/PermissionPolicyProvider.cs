b1a30eec992407b37738bb1af300db34d3a8ea5b
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2025-01-09 19:13:47 +0200

    Add custom authorization policy provider and requirement
    Introduce `Infrastructure.Identity.Auth` namespace with:
    - `PermissionPolicyProvider` implementing `IAuthorizationPolicyProvider`
      - Initializes `DefaultAuthorizationPolicyProvider`
      - Returns default policy, null fallback policy
      - Creates `AuthorizationPolicy` with `PermissionRequirement` based on policy name
    - `PermissionRequirement` implementing `IAuthorizationRequirement`
      - Stores required permission string

---
+++ Infrastructure/Identity/Auth/PermissionPolicyProvider.cs
@@ -0,0 +1,38 @@
+﻿namespace Infrastructure.Identity.Auth;
+
+using Infrastructure.Identity.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+internal class PermissionPolicyProvider : IAuthorizationPolicyProvider
+{
+    public DefaultAuthorizationPolicyProvider FallbackPolicyProvider { get; }
+
+    public PermissionPolicyProvider(IOptions<AuthorizationOptions> options)
+    {
+        FallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
+    }
+
+    public Task<AuthorizationPolicy> GetDefaultPolicyAsync()
+    {
+        return FallbackPolicyProvider.GetDefaultPolicyAsync();
+    }
+
+    public Task<AuthorizationPolicy> GetFallbackPolicyAsync()
+    {
+        return Task.FromResult<AuthorizationPolicy>(null);
+    }
+
+    public Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
+    {
+        if (policyName.StartsWith(ClaimConstants.Permission, StringComparison.OrdinalIgnoreCase)) 
+        {
+            var policy = new AuthorizationPolicyBuilder();
+            policy.AddRequirements(new PermissionRequirement(policyName));
+            return Task.FromResult<AuthorizationPolicy>(policy.Build());
+        }
+
+        return FallbackPolicyProvider.GetPolicyAsync(policyName);
+    }
+}
---
+++ Infrastructure/Identity/Auth/PermissionRequirement.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Identity.Auth;
+
+using Microsoft.AspNetCore.Authorization;
+
+internal class PermissionRequirement(string permission) : IAuthorizationRequirement
+{
+    public string Permission { get; set; } = permission;
+}
