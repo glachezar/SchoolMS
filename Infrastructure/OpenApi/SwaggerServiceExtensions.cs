ad55d434adfe59fdf618b5848a0fd5efb3f5cdf7
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2025-01-11 21:17:03 +0200

    Add OpenApi documentation extension method
    Introduced a new namespace `Infrastructure.OpenApi` and created an internal static class `SwaggerServiceExtensions`. Added an extension method `AddOpenApiDocumentation` to the `IServiceCollection` interface, which adds Swagger documentation services to the service collection. The method currently includes a placeholder for Swagger configuration and returns the modified service collection.

---
+++ Infrastructure/OpenApi/SwaggerServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.OpenApi;
+
+using Microsoft.Extensions.DependencyInjection;
+
+internal static class SwaggerServiceExtensions
+{
+    internal static IServiceCollection AddOpenApiDocumentation(this IServiceCollection services, SwagerSettings settings)
+    {
+        services.AddSwaggerDocument(config =>
+        {
+
+        });
+        return services;
+    }
+}
