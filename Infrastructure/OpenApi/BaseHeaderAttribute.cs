28de6481dccc67f30f7d23e89915fef292f0f88c
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2025-01-11 18:46:13 +0200

    Add NSwag package and BaseHeaderAttribute class
    Added NSwag.AspNetCore 14.0.3 to Infrastructure.csproj for OpenAPI support.
    Introduced BaseHeaderAttribute in Infrastructure.OpenApi namespace to
    document and enforce API header requirements.

---
+++ Infrastructure/OpenApi/BaseHeaderAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.OpenApi;
+
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class BaseHeaderAttribute : Attribute
+{
+    public string HeaderName { get; set; }
+    public string Description { get; set; }
+    public string DefaultValue { get; set; }
+    public bool IsRequired { get; set; }
+}
