dd9a55a3502edff889e78711fca90efae2277825
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2025-01-11 14:51:25 +0200

    Add ShouldHavePermissonAttribute for granular auth checks
    Introduce ShouldHavePermissonAttribute in Infrastructure.Identity.Auth
    namespace to enforce authorization policies based on actions and features.
    Update Infrastructure.csproj to include Identity\Auth\Jwt\ folder.

---
+++ Infrastructure/Identity/Auth/ShouldHavePermissonAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Identity.Auth;
+
+using Infrastructure.Identity.Constants;
+using Microsoft.AspNetCore.Authorization;
+
+public class ShouldHavePermissonAttribute : AuthorizeAttribute
+{
+    public ShouldHavePermissonAttribute( string action, string feature)
+    {
+        Policy = SchoolPermission.NameFor(action, feature);
+    }
+}
